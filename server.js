f66cf7967d68194f18e3dc8415b71219ab3ef3e2
Ibrahim Mahmoud <ibrahimibro99@gmail.com>
2019-11-20 05:24:08 +0000

    Patient now gets from SQL server.

--- server.js
+++ server.js
@@ -8,19 +8,23 @@
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
 var path = require('path');
-var fs = require('fs');
+var fs = require('fs'); /* this will be obsolete soon */
+var bodyParser = require('body-parser');
 var mysql = require('./dbcon.js');
-
-
 var app = express();
 
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.set('mysql', mysql);
 app.set('port', process.argv[2]);
 
 
-var patientArray = JSON.parse(fs.readFileSync("patient.json"));
+app.use('/patient', require('./patient.js'));
+
+//var patientArray = JSON.parse(fs.readFileSync("patient.json"));
 var medicationArray = JSON.parse(fs.readFileSync("medication.json"));
 var doctorArray = JSON.parse(fs.readFileSync("doctor.json"));
 var clinicArray = JSON.parse(fs.readFileSync("clinic.json"));
@@ -37,16 +41,11 @@ app.get('/home', function (req, res)
 	res.status(200).render('home');
 });
 
-app.get('/home.html', function (req, res)
- {
-	res.status(200).render('home');
-});
-
 app.get('/index.html', function (req, res)
 {
 	res.status(200).render('home');
 });
-
+/*
 app.get('/patient', function (req, res)
 {
 	res.status(200).render('patient',
@@ -54,56 +53,8 @@ app.get('/patient', function (req, res)
 		patientArray: JSON.parse(fs.readFileSync("patient.json"))
 	});
 });
-
-app.get('/patients', function (req, res)
-{
-	res.status(200).render('patient',
-	{
-		patientArray: JSON.parse(fs.readFileSync("patient.json"))
-	});
-});
-
-app.get('/patient.html', function (req, res)
-{
-	res.status(200).render('patient',
-	{
-		patientArray: JSON.parse(fs.readFileSync("patient.json"))
-	});
-});
-
-app.get('/patients.html', function (req, res)
-{
-	res.status(200).render('patient',
-	{
-		patientArray: JSON.parse(fs.readFileSync("patient.json"))
-	});
-});
-
+*/
 app.get('/medication', function (req, res)
-{
-	res.status(200).render('medication',
-	{
-		medicationArray: JSON.parse(fs.readFileSync("medication.json"))
-	});
-});
-
-app.get('/medications', function (req, res)
-{
-	res.status(200).render('medication',
-	{
-		medicationArray: JSON.parse(fs.readFileSync("medication.json"))
-	});
-});
-
-app.get('/medication.html', function (req, res)
-{
-	res.status(200).render('medication',
-	{
-		medicationArray: JSON.parse(fs.readFileSync("medication.json"))
-	});
-});
-
-app.get('/medications.html', function (req, res)
 {
 	res.status(200).render('medication',
 	{
@@ -119,55 +70,7 @@ app.get('/doctor', function (req, res)
 	});
 });
 
-app.get('/doctors', function (req, res)
-{
-	res.status(200).render('doctor',
-	{
-		doctorArray: JSON.parse(fs.readFileSync("doctor.json"))
-	});
-});
-
-app.get('/doctor.html', function (req, res)
-{
-	res.status(200).render('doctor',
-	{
-		doctorArray: JSON.parse(fs.readFileSync("doctor.json"))
-	});
-});
-
-app.get('/doctors.html', function (req, res)
-{
-	res.status(200).render('doctor',
-	{
-		doctorArray: JSON.parse(fs.readFileSync("doctor.json"))
-	});
-});
-
 app.get('/clinic', function (req, res)
-{
-	res.status(200).render('clinic',
-	{
-		clinicArray: JSON.parse(fs.readFileSync("clinic.json"))
-	});
-});
-
-app.get('/clinics', function (req, res)
-{
-	res.status(200).render('clinic',
-	{
-		clinicArray: JSON.parse(fs.readFileSync("clinic.json"))
-	});
-});
-
-app.get('/clinic.html', function (req, res)
-{
-	res.status(200).render('clinic',
-	{
-		clinicArray: JSON.parse(fs.readFileSync("clinic.json"))
-	});
-});
-
-app.get('/clinics.html', function (req, res)
 {
 	res.status(200).render('clinic',
 	{
@@ -183,38 +86,15 @@ app.get('/patient_medication', function (req, res)
 	});
 });
 
-app.get('/patients_medications', function (req, res)
-{
-	res.status(200).render('patient_medication',
-	{
-		patient_medicationArray: JSON.parse(fs.readFileSync("patient_medication.json"))
-	});
-});
-
-app.get('/patient_medication.html', function (req, res)
-{
-	res.status(200).render('patient_medication',
-	{
-		patient_medicationArray: JSON.parse(fs.readFileSync("patient_medication.json"))
-	});
-});
-
-app.get('/patients_medications.html', function (req, res)
-{
-	res.status(200).render('patient_medication',
-	{
-		patient_medicationArray: JSON.parse(fs.readFileSync("patient_medication.json"))
-	});
-});
-
 app.use(function(req,res)
 {
-  res.status(404).res.render('404');
+  res.status(404).render('404');
 });
+
 app.use(function(err, req, res, next)
 {
   console.error(err.stack);
-  res.status(500).res.render('500');
+  res.status(500).render('500');
 });
 
 app.listen(app.get('port'), function(){
